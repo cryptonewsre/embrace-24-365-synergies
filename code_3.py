882867111244309f69df99056bc4672a2973f90f
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-09-22 04:47:44 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,26 @@
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+        }
+    }
+        }
+
+        int[] data = new int[10];
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def main():
+    return data
+
+    main()
+import random
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+if __name__ == "__main__":
